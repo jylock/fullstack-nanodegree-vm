7449215808a3368e065adeefc92d56a898aa9a9e
Dustin Downs <dustin.downs@levatas.com>
2015-05-16 15:35:52 -0400

    Update forum.py and forumdb.py from Intro to RD class

--- vagrant/forum/forum.py
+++ vagrant/forum/forum.py
@@ -6,7 +6,7 @@
 import forumdb
 
 # Other modules used to run a web server.
-import cgi
+import urlparse
 from wsgiref.simple_server import make_server
 from wsgiref import util
 
@@ -21,7 +21,7 @@ HTML_WRAP = '''\
       textarea { width: 400px; height: 100px; }
       div.post { border: 1px solid #999;
                  padding: 10px 10px;
-		 margin: 10px 20%%; }
+                 margin: 10px 20%%; }
       hr.postbound { width: 50%%; }
       em.date { color: #999 }
     </style>
@@ -43,53 +43,55 @@ POST = '''\
     <div class=post><em class=date>%(time)s</em><br>%(content)s</div>
 '''
 
-## Request handler for main page
-def View(env, resp):
-    '''View is the 'main page' of the forum.
 
-    It displays the submission form and the previously posted messages.
-    '''
+# Request handler for main page
+def view(env, resp):
+    # View is the 'main page' of the forum.
+    # It displays the submission form and the previously posted messages.
+
     # get posts from database
-    posts = forumdb.GetAllPosts()
+    posts = forumdb.get_all_posts()
     # send results
     headers = [('Content-type', 'text/html')]
     resp('200 OK', headers)
     return [HTML_WRAP % ''.join(POST % p for p in posts)]
 
-## Request handler for posting - inserts to database
-def Post(env, resp):
-    '''Post handles a submission of the forum's form.
+
+# Request handler for posting - inserts to database
+def post(env, resp):
+    # Post handles a submission of the forum's form.
   
-    The message the user posted is saved in the database, then it sends a 302
-    Redirect back to the main page so the user can see their new post.
-    '''
+    # The message the user posted is saved in the database, then it sends a 302
+    # Redirect back to the main page so the user can see their new post.
+
     # Get post content
     input = env['wsgi.input']
     length = int(env.get('CONTENT_LENGTH', 0))
     # If length is zero, post is empty - don't save it.
     if length > 0:
         postdata = input.read(length)
-        fields = cgi.parse_qs(postdata)
+        fields = urlparse.parse_qs(postdata)
         content = fields['content'][0]
         # If the post is just whitespace, don't save it.
         content = content.strip()
         if content:
             # Save it in the database
-            forumdb.AddPost(content)
+            forumdb.add_post(content)
     # 302 redirect back to the main page
     headers = [('Location', '/'),
                ('Content-type', 'text/plain')]
     resp('302 REDIRECT', headers) 
     return ['Redirecting']
 
-## Dispatch table - maps URL prefixes to request handlers
-DISPATCH = {'': View,
-            'post': Post,
-	    }
+# Dispatch table - maps URL prefixes to request handlers
+DISPATCH = {'': view,
+            'post': post
+            }
 
-## Dispatcher forwards requests according to the DISPATCH table.
-def Dispatcher(env, resp):
-    '''Send requests to handlers based on the first path component.'''
+
+# Dispatcher forwards requests according to the DISPATCH table.
+def dispatcher(env, resp):
+    # Send requests to handlers based on the first path component.
     page = util.shift_path_info(env)
     if page in DISPATCH:
         return DISPATCH[page](env, resp)
@@ -101,7 +103,7 @@ def Dispatcher(env, resp):
 
 
 # Run this bad server only on localhost!
-httpd = make_server('', 8000, Dispatcher)
+httpd = make_server('', 8000, dispatcher)
 print "Serving HTTP on port 8000..."
 httpd.serve_forever()
 
--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,31 +1,28 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
-import time
+import psycopg2
+import bleach
 
-## Database connection
-DB = []
 
-## Get posts from database.
-def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
+# Get posts from database.
+def get_all_posts():
+    db = psycopg2.connect("dbname=forum")
+    cur = db.cursor()
 
-    Returns:
-      A list of dictionaries, where each dictionary has a 'content' key
-      pointing to the post content, and 'time' key pointing to the time
-      it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    cur.execute("SELECT time, content FROM posts ORDER BY time DESC")
+    posts = ({'content': str(row[1]), 'time': str(row[0])} for row in cur.fetchall())
+
+    db.close()
     return posts
 
-## Add a post to the database.
-def AddPost(content):
-    '''Add a new post to the database.
 
-    Args:
-      content: The text content of the new post.
-    '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+# Add a post to the database.
+def add_post(content):
+    db = psycopg2.connect("dbname=forum")
+    cur = db.cursor()
+
+    cur.execute("INSERT INTO posts (content) VALUES (%s)", (bleach.clean(content, strip=True),))
+    db.commit()
+    db.close()
