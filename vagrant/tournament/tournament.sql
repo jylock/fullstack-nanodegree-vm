1e72e228b259870b73793a20e5fea9dee2e28b27
Dustin Downs <dustin.downs@levatas.com>
2015-05-16 15:42:44 -0400

    Update tournament.sql with db and table creation

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,22 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Create tournament database
+CREATE DATABASE tournament;
 
+-- Connect to tournament database
+\c tournament;
+
+-- Create players table
+CREATE TABLE players (
+  name TEXT,
+  id SERIAL PRIMARY KEY
+);
+
+-- Create matches table
+CREATE TABLE matches (
+  p1 INTEGER REFERENCES players (id),
+  p2 INTEGER REFERENCES players (id),
+  winner INTEGER REFERENCES players (id),
+  PRIMARY KEY (p1, p2)
+);
