b349fbeabc7b022f392b6e1003fe778893ad0ae2
Dustin Downs <dustin.downs@levatas.com>
2015-05-17 11:00:09 -0400

    Add view for player standings

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -28,16 +28,23 @@ CREATE TABLE matches (
 
 -- Find the number of wins for each player
 CREATE VIEW player_wins AS
-SELECT players.id, players.name, COUNT(matches.winner) AS wins
+SELECT players.id, COUNT(matches.winner) AS wins
 FROM players LEFT JOIN matches
 ON players.id = matches.winner
 GROUP BY players.id
-ORDER BY wins DESC, players.id;
+ORDER BY wins DESC;
 
 -- Find the number of matches each player has played
 CREATE VIEW player_matches_played AS
-SELECT players.id, players.name, COUNT(matches.*) AS matches_played
+SELECT players.id, COUNT(matches.*) AS matches_played
 FROM players LEFT JOIN matches
 ON players.id = matches.p1 OR players.id = matches.p2
 GROUP BY players.id
-ORDER BY matches_played DESC, players.id;
+ORDER BY matches_played DESC;
+
+-- Player standings
+CREATE VIEW player_standings AS
+SELECT players.id, players.name, player_wins.wins, player_matches_played.matches_played
+FROM players, player_wins, player_matches_played
+WHERE players.id = player_wins.id AND players.id = player_matches_played.id
+ORDER BY player_wins.wins DESC, player_matches_played.matches_played, players.id;
