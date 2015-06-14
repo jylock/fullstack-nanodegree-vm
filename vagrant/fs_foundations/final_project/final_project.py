f6c3ec3b3634071d554bf97e32e5faf01cc8a4a2
Dustin Downs <dustin.downs@levatas.com>
2015-06-14 12:45:54 -0400

    Add templates

--- vagrant/fs_foundations/final_project/final_project.py
+++ vagrant/fs_foundations/final_project/final_project.py
@@ -75,32 +75,39 @@ def show_restaurants():
 
 @app.route("/restaurant/new")
 def new_restaurant():
-    return "This page will be for making a new restaurant"
+    # return "This page will be for making a new restaurant"
+    return render_template("new_restaurant.html")
 
 @app.route("/restaurant/<int:restaurant_id>/edit")
 def edit_restaurant(restaurant_id):
-    return "This page will be for editing restaurant %s" % restaurant_id
+    # return "This page will be for editing restaurant %s" % restaurant_id
+    return render_template("edit_restaurant.html", restaurant=restaurant)
 
 @app.route("/restaurant/<int:restaurant_id>/delete")
 def delete_restaurant(restaurant_id):
-    return "This page will be for deleting restaurant %s" % restaurant_id
+    # return "This page will be for deleting restaurant %s" % restaurant_id
+    return render_template("delete_restaurant.html", restaurant=restaurant)
 
 @app.route("/restaurant/<int:restaurant_id>")
 @app.route("/restaurant/<int:restaurant_id>/menu")
 def show_menu(restaurant_id):
-    return "This page is the menu for restaurant %s" % restaurant_id
+    # return "This page is the menu for restaurant %s" % restaurant_id
+    return render_template("menu.html", restaurant=restaurant, menu_items=items)
 
 @app.route("/restaurant/<int:restaurant_id>/menu/new")
 def new_menu_item(restaurant_id):
-    return "This page is for making a new menu item for restaurant %s" % restaurant_id
+    # return "This page is for making a new menu item for restaurant %s" % restaurant_id
+    return render_template("new_menu_item.html", restaurant=restaurant)
 
 @app.route("/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit")
 def edit_menu_item(restaurant_id, menu_id):
-    return "This page is for editing menu item %s" % menu_id
+    # return "This page is for editing menu item %s" % menu_id
+    return render_template("edit_menu_item.html", restaurant=restaurant, menu_item=item)
 
 @app.route("/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete")
 def delete_menu_item(restaurant_id, menu_id):
-    return "This page is for deleting menu item %s" % menu_id
+    # return "This page is for deleting menu item %s" % menu_id
+    return render_template("delete_menu_item.html", restaurant=restaurant, menu_item=item)
 
 if __name__ == "__main__":
     app.debug = True
